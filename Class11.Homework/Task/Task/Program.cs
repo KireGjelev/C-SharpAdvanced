29769bac6c0fbe85044d51e96e6dc8f0e5346074
KireGjelev <120602493+KireGjelev@users.noreply.github.com>
2023-05-10 21:05:56 +0200

    Add files via upload

---
+++ Class11.Homework/Task/Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task.Domain;
+
+Db<Dog> dogs = new Db<Dog>();
+
+List<Dog> readingFromFile = dogs.ReadFromFile();
+foreach (Dog dogsFromFile in readingFromFile)
+{
+    Console.WriteLine($"Name: {dogsFromFile.Name}, Age: {dogsFromFile.Age}, Color: {dogsFromFile.Color}");
+}
+
+Console.WriteLine("Enter the name of the dog:");
+string inputName = Console.ReadLine();
+
+Console.WriteLine("Enter how old is the dog");
+int inputAge = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the color of the dog");
+string inputColor = Console.ReadLine();
+
+
+Dog dog = new Dog(inputName, inputAge, inputColor);
+dogs.Insert(dog);
